251d463780917404354a8a133209d825cc35f155
Laura <theimer.laura@gmail.com>
2020-08-27 23:37:57 -0400

    menu set up correctly now.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Game from './components/game.js';
 function App() {
   return (
     <main>
-      <h1>Conway's Game of Life By Laura Theimer</h1> 
-      <h2> Game coming soon ... </h2>
-      {/* <Game /> */}
+      {/* <h1>Conway's Game of Life By Laura Theimer</h1> 
+      <h2> Game coming soon ... </h2> */}
+      <Game />
     </main>
   );
 }
---
+++ src/styling/menu.js
@@ -0,0 +1,26 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const menuStyles = makeStyles(theme => ({
+root: {
+},
+ruleMenu: {
+    marginTop: "-1%",
+    width: "250px",
+    backgroundColor: "lightgrey",
+    position: "fixed",
+    left: "84%"
+},
+aboutMenu: {
+    width: "350px",
+    position: "relative",
+    left: "25%",
+    marginTop: "-25%"
+},
+    innerMenu: {
+    backgroundColor: "black",
+    background: "linear-gradient(#000, #062474)",
+    color: "#90cccc",
+    padding: "10px",
+    boxShadow: "5px 10px 18px darkslategray" 
+}
+}));
